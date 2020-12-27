d0f2b7bc09e6528e0c14df82f172d3e7dbcf47bc
khushi  balyan <52583098+khushibalyan220101@users.noreply.github.com>
2020-12-27 21:17:51 +0530

    Create separateNumbers.java

---
+++ Algorithms/Strings/Separate the numbers/separateNumbers.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the separateNumbers function below.
+    static void separateNumbers(String str) {
+        int n = str.length();
+        int flag = 0;
+        String x = "";
+        String temp = "";
+        for(int i=1;i<=n/2;i++)
+        {
+            x = str.substring(0,i);
+            Long v = Long.parseLong(x);
+	    //loop till x's length is smaller than str's length
+            while(x.length()<n)
+            {
+                x+= Long.toString(++v);
+            }
+            if(x.equals(str))
+            {
+                temp = str.substring(0,i);
+                flag = 1;
+                break;
+            }
+        }
+        if(flag == 1){
+            System.out.println("YES "+temp);
+	}
+        else{
+            System.out.println("NO");
+	}
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = scanner.nextLine();
+
+            separateNumbers(s);
+        }
+
+        scanner.close();
+    }
+}
